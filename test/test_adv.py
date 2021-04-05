1297798a5be0d82e64fc438c685d02924ef7ec00
Matteo Rizzo <matteo.rizzo.work@gmail.com>
2021-04-05 15:25:06 +0200

    Condensed variance and adv test in one script

--- test/test_adv.py
+++ test/test_adv.py
@@ -3,6 +3,7 @@ import os
 import matplotlib.pyplot as plt
 import numpy as np
 import torch.utils.data
+from torch.utils.data import DataLoader
 
 from auxiliary.settings import DEVICE
 from auxiliary.utils import angular_error, jsd, scale
@@ -11,79 +12,88 @@ from classes.fc4.ModelAdvConfFC4 import ModelAdvConfFC4
 from classes.training.Evaluator import Evaluator
 
 NUM_FOLD = 0
-PATH_TO_PRETRAINED = os.path.join("trained_models", "adv", "fold_{}".format(NUM_FOLD))
+PATH_TO_PRETRAINED = os.path.join("trained_models")
+
+
+def evaluate(model: ModelAdvConfFC4, dataloader: DataLoader):
+    evaluator, evaluator_adv = Evaluator(), Evaluator()
+    eval_data = {"preds": [], "conf": [], "preds_adv": [], "conf_adv": []}
+
+    model.evaluation_mode()
+
+    with torch.no_grad():
+        for i, (img, label, file_name) in enumerate(dataloader):
+            img, label = img.to(DEVICE), label.to(DEVICE)
+            (pred, _, confidence), (pred_adv, _, confidence_adv) = model.predict(img)
+
+            loss = model.get_angular_loss(pred, label)
+            evaluator.add_error(loss.item())
+            eval_data["preds"].append(pred)
+            eval_data["conf"].append(torch.flatten(scale(confidence)).numpy())
+
+            loss_adv = model.get_angular_loss(pred_adv, label)
+            evaluator_adv.add_error(loss_adv.item())
+            eval_data["preds_adv"].append(pred_adv)
+            eval_data["conf_adv"].append(torch.flatten(scale(confidence_adv)).numpy())
+
+            print('\t - Input: {} - Batch: {} | Loss: {:.4f} - Loss Adv: {:.4f}'
+                  .format(file_name[0], i + 1, loss.item(), loss_adv.item()))
+
+        metrics_base, metrics_adv = evaluator.compute_metrics(), evaluator_adv.compute_metrics()
+        print(" \n Mean ........ : [ Base: {:.4f} | Adv: {:.4f} ]".format(metrics_base["mean"], metrics_adv["mean"]))
+        print(" Median ...... : [ Base: {:.4f} | Adv: {:.4f} ]".format(metrics_base["median"], metrics_adv["median"]))
+        print(" Trimean ..... : [ Base: {:.4f} | Adv: {:.4f} ]".format(metrics_base["trimean"], metrics_adv["trimean"]))
+        print(" Best 25% .... : [ Base: {:.4f} | Adv: {:.4f} ]".format(metrics_base["bst25"], metrics_adv["bst25"]))
+        print(" Worst 25% ... : [ Base: {:.4f} | Adv: {:.4f} ]".format(metrics_base["wst25"], metrics_adv["wst25"]))
+        print(" Worst 5% .... : [ Base: {:.4f} | Adv: {:.4f} ] \n".format(metrics_base["wst5"], metrics_adv["wst5"]))
+
+        err = [angular_error(pb, pa) for pb, pa in zip(eval_data["preds"], eval_data["preds_adv"])]
+        div = [jsd(cb, ca) for cb, ca in zip(eval_data["conf"], eval_data["conf_adv"])]
+
+        return np.mean(err), np.mean(div)
 
 
 def main():
-    errors, divergences = [], []
-    evaluator, evaluator_adv = Evaluator(), Evaluator()
-    for lambda_val in sorted(os.listdir(PATH_TO_PRETRAINED)):
-        fold_evaluator, fold_evaluator_adv = Evaluator(), Evaluator()
-        fold_eval_data = {"preds": [], "conf": [], "preds_adv": [], "conf_adv": []}
+    adv_data, var_data = {"errors": [], "divergences": []}, {"errors": [], "divergences": []}
+    test_set = ColorCheckerDataset(train=False, folds_num=NUM_FOLD)
+    dataloader = DataLoader(test_set, batch_size=1, shuffle=False, num_workers=20)
 
-        test_set = ColorCheckerDataset(train=False, folds_num=NUM_FOLD)
-        dataloader = torch.utils.data.DataLoader(test_set, batch_size=1, shuffle=False, num_workers=20)
+    print("\n *** Fold {} - Test set size: {} *** \n".format(NUM_FOLD, len(test_set)))
 
-        path_to_pretrained = os.path.join(PATH_TO_PRETRAINED, lambda_val)
+    base_path_to_pretrained = os.path.join(PATH_TO_PRETRAINED, "adv", "fold_{}".format(NUM_FOLD))
+    for model_dir in sorted(os.listdir(base_path_to_pretrained)):
+        print("\n -> Lambda: {}".format("0." + model_dir.split("L")[1]))
+
+        path_to_pretrained = os.path.join(base_path_to_pretrained, model_dir)
+        print("\n Using pretrained model stored at: {} \n".format(path_to_pretrained))
+
         model = ModelAdvConfFC4()
         model.load(path_to_pretrained)
-        model.evaluation_mode()
 
-        lambda_val = float("0." + lambda_val.split("L")[1])
-        print("\n *** FOLD {} - LAMBDA: {} *** \n".format(NUM_FOLD, lambda_val))
-        print(" * Test set size: {}".format(len(test_set)))
-        print(" * Using pretrained model stored at: {} \n".format(path_to_pretrained))
+        err, div = evaluate(model, dataloader)
+        adv_data["errors"].append(err)
+        adv_data["divergences"].append(div)
 
-        with torch.no_grad():
-            for i, (img, label, file_name) in enumerate(dataloader):
-                img, label = img.to(DEVICE), label.to(DEVICE)
-                (pred, _, confidence), (pred_adv, _, confidence_adv) = model.predict(img)
+    base_path_to_pretrained = os.path.join(PATH_TO_PRETRAINED, "variance", "fold_{}".format(NUM_FOLD))
+    for model_dir in sorted(os.listdir(base_path_to_pretrained)):
+        print("\n -> Seed: {}".format(model_dir.split("_")[1]))
 
-                loss = model.get_angular_loss(pred, label)
-                fold_evaluator.add_error(loss.item())
-                evaluator.add_error(loss.item())
-                fold_eval_data["preds"].append(pred)
-                fold_eval_data["conf"].append(torch.flatten(scale(confidence)).numpy())
+        path_to_pretrained = os.path.join(base_path_to_pretrained, model_dir)
+        print("\n Using pretrained model stored at: {} \n".format(path_to_pretrained))
 
-                loss_adv = model.get_angular_loss(pred_adv, label)
-                fold_evaluator_adv.add_error(loss_adv.item())
-                evaluator_adv.add_error(loss_adv.item())
-                fold_eval_data["preds_adv"].append(pred_adv)
-                fold_eval_data["conf_adv"].append(torch.flatten(scale(confidence_adv)).numpy())
+        model = ModelAdvConfFC4()
+        model.load(path_to_pretrained)
 
-                print('\t - Input: {} - Batch: {} | Loss: {:.4f} - Loss Adv: {:.4f}'
-                      .format(file_name[0], i + 1, loss.item(), loss_adv.item()))
+        err, div = evaluate(model, dataloader)
+        var_data["errors"].append(err)
+        var_data["divergences"].append(div)
 
-        metrics, metrics_adv = fold_evaluator.compute_metrics(), fold_evaluator_adv.compute_metrics()
-        print("\n Mean ...... : [ Base: {:.4f} | Adv: {:.4f} ]".format(metrics["mean"], metrics_adv["mean"]))
-        print(" Median ...... : [ Base: {:.4f} | Adv: {:.4f} ]".format(metrics["median"], metrics_adv["median"]))
-        print(" Trimean ..... : [ Base: {:.4f} | Adv: {:.4f} ]".format(metrics["trimean"], metrics_adv["trimean"]))
-        print(" Best 25% .... : [ Base: {:.4f} | Adv: {:.4f} ]".format(metrics["bst25"], metrics_adv["bst25"]))
-        print(" Worst 25% ... : [ Base: {:.4f} | Adv: {:.4f} ]".format(metrics["wst25"], metrics_adv["wst25"]))
-        print(" Worst 5% .... : [ Base: {:.4f} | Adv: {:.4f} ] \n".format(metrics["wst5"], metrics_adv["wst5"]))
-
-        err = [angular_error(pb, pa) for pb, pa in zip(fold_eval_data["preds"], fold_eval_data["preds_adv"])]
-        errors.append(np.mean(err))
-
-        div = [jsd(cb, ca) for cb, ca in zip(fold_eval_data["conf"], fold_eval_data["conf_adv"])]
-        divergences.append(np.mean(div))
-
-    print("div: {} \n err: {}".format(divergences, errors))
-
-    plt.plot(divergences, errors, linestyle='--', marker='o', color='b')
+    plt.plot(adv_data["divergences"], adv_data["errors"], linestyle='--', marker='o', color='r')
+    plt.scatter(var_data["divergences"], var_data["errors"], marker='^', color='g')
     plt.xlabel("Confidence JSD")
-    plt.ylabel("Angular error")
+    plt.ylabel("Predictions AE")
+    plt.savefig(os.path.join("test", "adv.png"), bbox_inches='tight')
     plt.show()
-    plt.savefig(os.path.join("test", "adv_lambda.png"), bbox_inches='tight')
-
-    print("\n *** AVERAGE ACROSS FOLDS *** \n")
-    metrics, metrics_adv = evaluator.compute_metrics(), evaluator_adv.compute_metrics()
-    print("\n Mean ...... : [ Base: {:.4f} | Adv: {:.4f} ]".format(metrics["mean"], metrics_adv["mean"]))
-    print(" Median ...... : [ Base: {:.4f} | Adv: {:.4f} ]".format(metrics["median"], metrics_adv["median"]))
-    print(" Trimean ..... : [ Base: {:.4f} | Adv: {:.4f} ]".format(metrics["trimean"], metrics_adv["trimean"]))
-    print(" Best 25% .... : [ Base: {:.4f} | Adv: {:.4f} ]".format(metrics["bst25"], metrics_adv["bst25"]))
-    print(" Worst 25% ... : [ Base: {:.4f} | Adv: {:.4f} ]".format(metrics["wst25"], metrics_adv["wst25"]))
-    print(" Worst 5% .... : [ Base: {:.4f} | Adv: {:.4f} ] \n".format(metrics["wst5"], metrics_adv["wst5"]))
 
 
 if __name__ == '__main__':
