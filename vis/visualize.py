445d1e9a985fe108ec5fde858439bc2cc1ecd9c8
Matteo Rizzo <matteo.rizzo.phd@gmail.com>
2021-07-05 10:09:49 +0200

    Set default path to pretrained

--- vis/visualize.py
+++ vis/visualize.py
@@ -32,8 +32,7 @@ def main():
         test_set = ColorCheckerDataset(train=False, folds_num=num_fold)
         dataloader = DataLoader(test_set, batch_size=1, shuffle=False, num_workers=20)
 
-        # path_to_pretrained = os.path.join("trained_models", "baseline", "fc4_cwp", "fold_{}".format(num_fold))
-        path_to_pretrained = os.path.join("trained_models/variance/seed_0/")
+        path_to_pretrained = os.path.join("trained_models", "baseline", "fc4_cwp", "fold_{}".format(num_fold))
         model.load(path_to_pretrained)
         model.evaluation_mode()
 
